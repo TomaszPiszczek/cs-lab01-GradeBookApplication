335821d6210bea2ba3e410683138ee8b6a126090
Tomasz <Tomasz.Piszczek@microsoft.wsei.edu.pl>
2023-03-14 18:57:19 +0100

    3rd task completed CreateStandardGradeBookandRankedGradeBookClassesTests passed

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(String name):base(name) 
+        {
+            base.Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
